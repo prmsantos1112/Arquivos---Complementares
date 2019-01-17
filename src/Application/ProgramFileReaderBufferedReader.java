699f403098f30a35638bec012ae7c8ff66868fe2
prmsantos1112 <prmsantosster@gmail.com>
2019-01-17 19:38:28 -0200

    File Reader Buffered Reader - Java

---
+++ src/Application/ProgramFileReaderBufferedReader.java
@@ -0,0 +1,42 @@
+package Application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ProgramFileReaderBufferedReader {
+
+	public static void main(String[] args) {
+
+		String path = "C:\\Users\\prmsa\\eclipse-workspace_2018-09\\documento.txt";
+		
+		FileReader fr = null;
+		BufferedReader br = null;
+		try {
+			fr = new FileReader(path);
+			br = new BufferedReader(fr);
+			
+			String line = br.readLine();
+			
+			while (line != null) {
+				System.out.println(line);
+				line = br.readLine();
+			}
+		} 
+		catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		} 
+		finally {
+			try 
+			{
+				if (br != null)
+					br.close();
+				if (fr != null)
+					fr.close();
+			} 
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
